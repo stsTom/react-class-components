fe61ebdb74e82b8b846ec2faa6bebb3876a44387
stsTom <tamilaustsiapan@gmail.com>
2026-05-11 18:55:47 +0200

    Search component tests are written

---
+++ src/tests/components/Search/search.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Search } from '../../../components/Search/Search';
+import SearchContext from '../../../context/SearchContext';
+import { type SearchContextType } from '../../../context/SearchContext';
+
+describe('Search functionality', () => {
+  it('calls findItems from context when search request is submitted', async () => {
+    const findItems = vi.fn();
+    const user = userEvent.setup();
+
+    render(
+      <SearchContext.Provider
+        value={
+          { findItems, simulateError: vi.fn() } as unknown as SearchContextType
+        }
+      >
+        <Search />
+      </SearchContext.Provider>
+    );
+
+    await user.type(screen.getByPlaceholderText('Search'), 'Star Trek');
+    await user.click(screen.getByRole('button', { name: /^search$/i }));
+
+    expect(findItems).toHaveBeenCalledWith('Star Trek');
+  });
+});
