587c3b264fdd10142c691d2d1f107a41532156d7
stsTom <tamilaustsiapan@gmail.com>
2026-05-11 16:51:15 +0200

    Set up vitest and RTL for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
+/// <reference types="@testing-library/jest-dom" />
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+      thresholds: {
+        lines: 50,
+        functions: 50,
+        branches: 50,
+        statements: 80,
+      },
+    },
+  },
 });
